4e97cfc7bf4cfc31fc61fc1990e49b3ad51209c2
QuanT Ubuntu <quan.td55@gmail.com>
2020-09-30 11:44:18 +0700

    register and validate register

--- api/routes/usersRoute.js
+++ api/routes/usersRoute.js
@@ -1,24 +1,43 @@
 const express = require("express"),
   User = require("../models/user"),
   router = express.Router(),
-  Joi = require("@hapi/joi");
+  bcrypt = require("bcryptjs"),
+  { registerValidate, loginValidate } = require("../../validation");
 
-let schema = {
-  fullname: Joi.string().required(),
-  email: Joi.string().required().email(),
-  password: Joi.string().required(),
-};
 router.post("/register", async (req, res) => {
+  // validate data
+  const { error } = registerValidate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  //check exist user
+  const userExist = await User.findOne({ email: req.body.email });
+  if (userExist) return res.status(400).send("Email already exists");
+
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   let user = new User({
     fullname: req.body.fullname,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
+
   try {
     let savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: savedUser._id });
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+router.post("/login", (req, res) => {
+  // validate data
+  const { error } = loginValidate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
   }
 });
 
